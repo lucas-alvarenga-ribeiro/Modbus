98b7a601dacdbe3acc4a77d356fc4757188b56db
Lucas Ribeiro <lucasalvarengaribeiro.lar@gmail.com>
2026-05-21 19:50:26 -0300

    Update float value and print messages in exemplo_float.py

--- exemplo_float.py
+++ exemplo_float.py
@@ -4,17 +4,13 @@ def main():
     cliente = ClienteMODBUS('localhost', 502)
     if cliente.connect():
         addr = 100
-        val = 123.45
-        
-        print(f"Escrevendo float {val} no endereço {addr}...")
+        val = 40.5
+        print(f"Escrevendo valor float {val} no seguinte endereço {addr}...")
         cliente.escreveFloat(addr, val)
-        
         lido = cliente.lerFloat(addr)
         print(f"Valor lido: {lido}")
-        
         cliente.close()
     else:
         print("Sem conexão")
-
 if __name__ == '__main__':
     main()
